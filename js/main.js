7ac6e72a2f25922bbba82d5ab7529778d226c636
iciteam123 <iciteam123@gmail.com>
2025-05-28 21:13:45 -0700

    Update about page

--- js/main.js
+++ js/main.js
@@ -1,70 +1,78 @@
-$('.slick-slider').slick({
-    slidesToShow: 7,
-    SlidesToScroll: 1,
-    dots: false,
-    arrows: true,
-    autoplay: true,
-    autoplaySpeed: 2000, 
-    infinite: true
+$(".slick-slider").slick({
+  slidesToShow: 7,
+  SlidesToScroll: 1,
+  dots: false,
+  arrows: true,
+  autoplay: true,
+  autoplaySpeed: 2000,
+  infinite: true,
 });
 
-
-$(document).ready(function(){
-    $('.counter-value').each(function(){
-        $(this).prop('Counter',0).animate({
-            Counter: $(this).text()
-        },{
-            duration: 3500,
-            easing: 'swing',
-            step: function (now){
-                $(this).text(Math.ceil(now));
-            }
-        });
-    });
+$(document).ready(function () {
+  $(".counter-value").each(function () {
+    $(this)
+      .prop("Counter", 0)
+      .animate(
+        {
+          Counter: $(this).text(),
+        },
+        {
+          duration: 3500,
+          easing: "swing",
+          step: function (now) {
+            $(this).text(Math.ceil(now));
+          },
+        }
+      );
+  });
 });
 
-$.fn.jQuerySimpleCounter = function(options) {
-    var settings = $.extend({
+$.fn.jQuerySimpleCounter = function (options) {
+  var settings = $.extend(
+    {
       start: 0,
       end: 100,
-      easing: 'swing',
+      easing: "swing",
       duration: 400,
-      complete: ''
-    }, options);
-  
-    var thisElement = $(this);
-  
-    $({
-      count: settings.start
-    }).animate({
-      count: settings.end
-    }, {
+      complete: "",
+      suffix: "",
+    },
+    options
+  );
+
+  var thisElement = $(this);
+
+  $({
+    count: settings.start,
+  }).animate(
+    {
+      count: settings.end,
+    },
+    {
       duration: settings.duration,
       easing: settings.easing,
-      step: function() {
-        var mathCount = Math.ceil(this.count);
-        thisElement.text(mathCount);
+      step: function () {
+        var mathCount = Math.ceil(Math.min(this.count, settings.maxNumber));
+        thisElement.text(mathCount + settings.suffix);
       },
-      complete: function() {
-        var mathCount = this.count > settings.maxNumber ? settings.maxNumber + '+' : this.count;
-        thisElement.text(mathCount);
-      }
-  
-    });
-  };
-  
-  $('#number1').jQuerySimpleCounter({
-    end: 201,
+      complete: function () {
+        var mathCount = Math.min(this.count, settings.maxNumber);
+
+        let suffix = settings.suffix;
+        if (this.count > settings.maxNumber) {
+          suffix += "+";
+        }
+        thisElement.text(mathCount + suffix);
+      },
+    }
+  );
+};
+
+$(".number-counter").each(function (index, element) {
+  $(element).jQuerySimpleCounter({
+    end: parseInt($(element).attr("data-count") || 0) + 1,
     duration: 3000,
-    maxNumber: 200
+    maxNumber: parseInt($(element).attr("data-count") || 0),
+    suffix: $(element).attr("data-suffix") || "",
   });
-  $('#number2').jQuerySimpleCounter({
-    end: 5001,
-    duration: 3000,
-    maxNumber: 5000
-  });
-  $('#number3').jQuerySimpleCounter({
-    end: 20001,
-    duration: 3000,
-    maxNumber: 20000
-  });
+});
